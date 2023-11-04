c56376c288439ae051eb59e224d662b0d8c5e462
SemicolonUnexpected <112770950+SemicolonUnexpected@users.noreply.github.com>
2023-11-04 16:00:10 +0000

    Added spinning loading icon

--- Tree Lights GUI/View/SplashScreenView.xaml.cs
+++ Tree Lights GUI/View/SplashScreenView.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace Tree_Lights_GUI.View
-{
+namespace Tree_Lights_GUI.View {
     /// <summary>
     /// Interaction logic for SplashScreenView.xaml
     /// </summary>
@@ -28,6 +16,10 @@ namespace Tree_Lights_GUI.View
             if(e.LeftButton == MouseButtonState.Pressed) {
                 DragMove();
             }
+        }
+
+        private void LoadingSpinner_Loaded(object sender, RoutedEventArgs e) {
+
         }
     }
 }
